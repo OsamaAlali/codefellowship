2340a0b3b653474cea2930e2578f0916e6a6cc1c
OsamaAlali <osama.alali89@yahoo.com>
2021-08-16 22:56:35 +0300

    lab 17 done

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 
 - create user accounts using Spring Auth.
 -  authenticate users with Spring Auth.
+- create a social app where users can create their own content for others to view publicly
--- codefellowship/src/main/java/com/example/codefellowship/controller/UserController.java
+++ codefellowship/src/main/java/com/example/codefellowship/controller/UserController.java
@@ -1,5 +1,8 @@
 package com.example.codefellowship.controller;
 
+import com.example.codefellowship.model.Post;
+import com.example.codefellowship.repository.ApplicationUserRepository;
+import com.example.codefellowship.repository.PostRepository;
 import com.example.codefellowship.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
@@ -7,6 +10,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.view.RedirectView;
 
 import java.security.Principal;
 
@@ -16,13 +22,24 @@ public class UserController {
 @Autowired
     UserRepository userRepository;
 
+@Autowired
+    ApplicationUserRepository applicationUserRepository;
+@Autowired
+PostRepository postRepository;
 
-@GetMapping("/users/{id}")
-    public String getUserInfo(Model m, @PathVariable Integer id, Principal p) {
-//    m.addAttribute("username",p.getName());
-    m.addAttribute("user",userRepository.findById(id));
+@GetMapping("/users")
+    public String getUserInfo(Model m,  Principal p) {
+    m.addAttribute("username",p.getName());
+    m.addAttribute("user",applicationUserRepository.findByUsername(p.getName()));
     return "users.html";
 
+}
+
+@PostMapping("/users")
+    public RedirectView addPost(Principal p,@RequestParam String body){
+    Post newPost= new Post(body,applicationUserRepository.findByUsername(p.getName()));
+      postRepository.save(newPost);
+      return new RedirectView("/users");
 }
 
 }
